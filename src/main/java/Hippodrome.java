fd6f2c323ca43fef9e4eb704a86c768c86374408
Meeeat <angroov@gmail.com>
2024-11-11 20:12:31 +0900

    ADD logger in main and hippo class. add dependencies for sl4j

--- src/main/java/Hippodrome.java
+++ src/main/java/Hippodrome.java
@@ -1,3 +1,6 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -5,7 +8,7 @@ import java.util.List;
 import static java.util.Objects.isNull;
 
 public class Hippodrome {
-
+    private static final Logger logger = LoggerFactory.getLogger(Hippodrome.class);
     private final List<Horse> horses;
 
     public Hippodrome(List<Horse> horses) {
